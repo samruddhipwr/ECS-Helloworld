dad826a700900e0751fd964b667ab63c846c7139
samruddhipwr <69259493+samruddhipwr@users.noreply.github.com>
2022-10-18 22:44:08 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,126 @@
+resource "aws_ecr_repository" "hello-world" {
+  name                 = "hello-world"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecs_cluster" "hello-world-cl" {
+  name = "hello-world"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+
+resource "aws_ecs_task_definition" "first-task" {
+ depends_on = [
+    aws_iam_role.ecsTaskExecutionRole
+  ]
+ 
+  family = "first-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 1024
+  memory                   = 2048
+  execution_role_arn = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+  container_definitions = jsonencode([
+    {
+      name      = "first"
+      image     = "${aws_ecr_repository.hello-world.repository_url}",
+      essential = true
+      portMappings = [
+        { 
+          protocol = "tcp"
+          containerPort = 80
+          hostPort      = 80
+        }
+      ],
+     logConfiguration={
+          "logDriver": "awslogs",
+          "options": {
+            "awslogs-create-group": "true",
+            "awslogs-group": "/ecs/app",
+            "awslogs-region": "eu-west-2",
+            "awslogs-stream-prefix": "ecs"
+          }
+        }
+    }
+  ]
+  )
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole"{
+  name= "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+  inline_policy {
+    name = "my_inline_policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["*"]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+}
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "assume_role_policy"{
+  statement {
+    sid = "A"
+    actions=["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+
+    }
+}
+  statement {
+    sid= "B"
+    actions=["sts:AssumeRole"]
+
+    principals {
+      type = "AWS"
+      identifiers = ["*"]
+
+    }
+  }
+}
+
+resource "aws_ecs_service" "hello-world" {
+  name                = "hello-world"
+  cluster             = aws_ecs_cluster.hello-world-cl.id
+  task_definition     = aws_ecs_task_definition.first-task.arn
+  launch_type = "FARGATE"
+  desired_count = 2
+
+  network_configuration {
+    subnets= ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
+    assign_public_ip = true 
+  }
+}
+
+resource "aws_default_vpc" "default_vpc" {
+  
+}
+
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "eu-west-2a"
+  
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "eu-west-2b"
+  
+}
+ 
